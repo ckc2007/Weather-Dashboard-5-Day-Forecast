1d6c18bf7133df9ff3261e176cf163ab02d27a5f
ckc2007 <ckc2007@gmail.com>
2023-04-02 18:46:59 -0400

    create the current weather display

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ var forecastEl = $("#forecast");
 var searchHistoryListEl = $("#search-history");
 
 // put this here so you dont need to copy and paste it in each time
-var myApiKey = "c9f4436f54acb5291e5113e098327c64";
+var myApiKey = `c9f4436f54acb5291e5113e098327c64`;
 
 // empty array to hold search history
 var searchHistoryArr = [];
@@ -18,6 +18,7 @@ formEl.on("submit", (event) => {
   // what are we getting?
   // get the user input value
   var city = cityInput.value;
+  console.log(city);
 
   // need to call the api now and enter the city to the query parameter
   // use fetch?
@@ -25,10 +26,32 @@ formEl.on("submit", (event) => {
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${myApiKey}`
   )
     .then(function (response) {
+      // use an arrow function here maybe? looks ugly like this...
       return response.json();
     })
     .then(function (data) {
-      var currentWeather = {};
-      
+      console.log(data);
+      //   need current and future conditions:
+      //   we need city name, date, icon, temperature, humidity and wind speed
+      var currentWeather = {
+        city: data.name,
+        date: new Date(data.dt * 1000),
+        icon: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`,
+        temperature: data.main.temp,
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+      };
+
+      //   show current weather data
+      //   create a paragraph, or maybe make a card and asign innerText - for now <p>
+      currentWeatherEl.innerHTML = `
+    <h3>${
+      currentWeather.city
+    } ${currentWeather.date.toLocaleDateString()} <img src="${
+        currentWeather.icon
+      }"></h2>
+    <p>Temperature: ${currentWeather.temperature} degrees Farenheit</p>
+    <p>Humidity: ${currentWeather.humidity}%</p>
+    <p>Wind Speed: ${currentWeather.windSpeed} MPH</p>`;
     });
 });
