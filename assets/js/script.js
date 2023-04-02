ef9994cf7e247cf4e11fee5ca13a3c59d35cf5ec
ckc2007 <ckc2007@gmail.com>
2023-04-02 18:20:56 -0400

    added city variable for input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // set up variables to target html elements
-var form = $("#search-form");
+var formEl = $("#search-form");
 var cityInput = $("#city-input");
 var currentWeatherEl = $("#current-weather");
 var forecastEl = $("#forecast");
@@ -9,7 +9,15 @@ var searchHistoryListEl = $("#search-history");
 var searchHistoryArr = [];
 
 // event listener for the search form submit button
-form.on("submit", (event)=>{
+formEl.on("submit", (event)=>{
     // remember to do this to prevent auto clearing and reload
     event.preventDefault();
+    // what are we getting?
+    // get the user input value
+    var city = cityInput.value;
+
+    // need to call the api now and enter the city to the query parameter
+    // use fetch?
+
+
 })
