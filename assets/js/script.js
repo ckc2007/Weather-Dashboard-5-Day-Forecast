19936b5508c5d7ed54229a635f5c73271514306c
ckc2007 <ckc2007@gmail.com>
2023-04-02 18:14:12 -0400

    added js variables to target html id tags for dynamic elements

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// set up variables to target html elements
+var form = $("#search-form");
+var cityInput = $("#city-input");
+var currentWeatherEl = $("#current-weather");
+var forecastEl = $("#forecast");
+var searchHistoryListEl = $("#search-history");
